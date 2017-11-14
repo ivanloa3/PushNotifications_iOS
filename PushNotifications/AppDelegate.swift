973922592e41e14b0bd17fcaf7063d1ea10e1214
Ivan Lopez <ivan_loa3@hotmail.com>
2017-11-14 09:37:42 -0600

    first commit uploading app PushNotifications

--- PushNotifications/AppDelegate.swift
+++ PushNotifications/AppDelegate.swift
@@ -7,87 +7,105 @@
 //
 
 import UIKit
-import CoreData
+import Firebase
+import FirebaseMessaging
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
-    }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "PushNotifications")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate{
+        
+        var window: UIWindow?
+        
+        
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+            // Override point for customization after application launch.
+            //create the notificationCenter
+            if #available(iOS 10.0, *) {
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.current().delegate = self
+                
+                let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+                UNUserNotificationCenter.current().requestAuthorization(
+                    options: authOptions,
+                    completionHandler: {_, _ in })
+                
+                // For iOS 10 data message (sent via FCM)
+                //FIRMessaging.messaging().remoteMessageDelegate = self
+                
+            } else {
+                let settings: UIUserNotificationSettings =
+                    UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+                application.registerUserNotificationSettings(settings)
             }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+            
+            application.registerForRemoteNotifications()
+            
+            FirebaseApp.configure()
+            
+            return true
         }
-    }
-
+        
+        func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+            var token = ""
+            for i in 0..<deviceToken.count {
+                token = token + String(format: "%02.2hhx", arguments: [deviceToken[i]])
+            }
+            print("Registration succeeded! Token: ", token)
+        }
+        
+        func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+            print("Registration failed!")
+        }
+        
+        func applicationWillResignActive(_ application: UIApplication) {
+            // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+            // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        }
+        
+        func applicationDidEnterBackground(_ application: UIApplication) {
+            // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+            // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        }
+        
+        func applicationWillEnterForeground(_ application: UIApplication) {
+            // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        }
+        
+        func applicationDidBecomeActive(_ application: UIApplication) {
+            // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        }
+        
+        func applicationWillTerminate(_ application: UIApplication) {
+            // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        }
+        
+        // Firebase notification received
+        @available(iOS 10.0, *)
+        func userNotificationCenter(_ center: UNUserNotificationCenter,  willPresent notification: UNNotification, withCompletionHandler   completionHandler: @escaping (_ options:   UNNotificationPresentationOptions) -> Void) {
+            
+            // custom code to handle push while app is in the foreground
+            print("Handle push from foreground\(notification.request.content.userInfo)")
+            
+            let dict = notification.request.content.userInfo["aps"] as! NSDictionary
+            let d : [String : Any] = dict["alert"] as! [String : Any]
+            let body : String = d["body"] as! String
+            let title : String = d["title"] as! String
+            print("Title:\(title) + body:\(body)")
+            self.showAlertAppDelegate(title: title,message:body,buttonTitle:"ok",window:self.window!)
+            
+        }
+        
+        @available(iOS 10.0, *)
+        func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+            // if you set a member variable in didReceiveRemoteNotification, you  will know if this is from closed or background
+            print("Handle push from background or closed\(response.notification.request.content.userInfo)")
+        }
+        
+        func showAlertAppDelegate(title: String,message : String,buttonTitle: String,window: UIWindow){
+            let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: buttonTitle, style: UIAlertActionStyle.default, handler: nil))
+            window.rootViewController?.present(alert, animated: false, completion: nil)
+        }
+        // Firebase ended here
+        
 }
-
--- PushNotifications/ViewController.swift
+++ PushNotifications/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label_pmam: UILabel!
+    @IBOutlet weak var label_time: UILabel!
+    
+    var timer : Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateClock()
+        timer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(ViewController.updateClock), userInfo: nil, repeats: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func updateClock(){
+        let date = Date()
+        let calendar = Calendar.current
+        let requestedComponents : NSCalendar.Unit = [NSCalendar.Unit.hour, NSCalendar.Unit.minute]
+        let components = (calendar as NSCalendar).components(requestedComponents, from: date)
+        var hour = components.hour
+        let minute = components.minute
+        
+        if hour! > 12{
+            hour = hour! - 12
+            label_pmam.text = "PM"
+        }else{
+            label_pmam.text = "AM"
+        }
+        
+        label_time.text = padZero(hour!) + ":" + padZero(minute!)
+        
     }
 
-
+    func padZero(_ numb : Int) -> String{
+        let numb2 = (numb < 10 ? "0" : "") + String(numb)
+        return numb2
+    }
 }
 
